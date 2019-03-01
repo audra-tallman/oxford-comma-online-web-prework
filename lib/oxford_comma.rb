ee58cbdbe91e9832041e653fa7ff9a4795af9ca5
Audra Tallman <audra.tallman@hotmail.com>
2019-03-01 23:01:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
-array = []
+
 def oxford_comma(array)
  if array.length == 1 
     array.join
@@ -8,7 +8,7 @@ def oxford_comma(array)
     array.join
   end
   if array.length >2
-    array[-1].prepend "and" 
-    array.join (",")
+    array[-1].prepend "and " 
+    array.join (", ")
   end
 end
