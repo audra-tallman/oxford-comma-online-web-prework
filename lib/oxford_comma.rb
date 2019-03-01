372a7c92d97a3b9f6e2365202777293241e745e3
Audra Tallman <audra.tallman@hotmail.com>
2019-03-01 22:59:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+array = []
 def oxford_comma(array)
+ if array.length == 1 
+    array.join
+  end
+ if array.length == 2
+    array[-2] << "and"
+    array.join
+  end
+  if array.length >2
+    array[-1].prepend "and" 
+    array.join (",")
+  end
 
+oxford_comma(array)
 end
